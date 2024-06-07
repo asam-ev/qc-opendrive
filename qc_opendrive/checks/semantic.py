fe6e9b4ec0f509bf9d1aeb9c97747b0bbeac8555
patrickpa <patrick@ivex.ai>
2024-06-07 11:16:33 +0200

    Fix logic on check to address not only first match on float
    Signed-off-by: patrickpa <patrick@ivex.ai>

--- qc_opendrive/checks/semantic.py
+++ qc_opendrive/checks/semantic.py
@@ -16,21 +16,7 @@ CHECKER_ID = "semantic_xodr"
 @dataclass
 class SOffsetInfo:
     s_offset: float
-    rules: Set
-
-
-def _get_s_offset_info(
-    access_s_offset_info: List[SOffsetInfo], s_offset: float
-) -> Union[SOffsetInfo, None]:
-
-    return next(
-        (
-            s_offset_info
-            for s_offset_info in access_s_offset_info
-            if abs(s_offset_info.s_offset - s_offset) <= 1e-6
-        ),
-        None,
-    )
+    rule: str
 
 
 # TODO: Add logic to handle that this rule only applied to xord 1.7 and 1.8
@@ -57,37 +43,43 @@ def check_invalid_road_lane_access_no_mix_of_deny_or_allow(
             access_attr = access.attrib
 
             if "rule" in access_attr:
-                s_off_set_info = _get_s_offset_info(
-                    access_s_offset_info=access_s_offset_info,
-                    s_offset=float(access_attr["sOffset"]),
-                )
+                s_offset = float(access_attr["sOffset"])
+                rule = access_attr["rule"]
 
-                if s_off_set_info is None:
-                    access_s_offset_info.append(
-                        SOffsetInfo(
-                            s_offset=float(access_attr["sOffset"]),
-                            rules=set([access_attr["rule"]]),
+                for s_offset_info in access_s_offset_info:
+                    if (
+                        abs(s_offset_info.s_offset - s_offset) <= 1e-6
+                        and rule != s_offset_info.rule
+                    ):
+                        result.register_issue(
+                            checker_bundle_name=constants.BUNDLE_NAME,
+                            checker_id=CHECKER_ID,
+                            issue_id=issue_count,
+                            description="At a given s-position, either only deny or only allow values shall be given, not mixed.",
+                            level=IssueSeverity.ERROR,
                         )
+
+                        path = root.getpath(access)
+
+                        previous_rule = s_offset_info.rule
+                        current_rule = access_attr["rule"]
+
+                        result.add_xml_location(
+                            checker_bundle_name=constants.BUNDLE_NAME,
+                            checker_id=CHECKER_ID,
+                            issue_id=issue_count,
+                            xpath=path,
+                            description=f"First encounter of {current_rule} having {previous_rule} before.",
+                        )
+
+                        issue_count += 1
+
+                access_s_offset_info.append(
+                    SOffsetInfo(
+                        s_offset=float(access_attr["sOffset"]),
+                        rule=access_attr["rule"],
                     )
-                elif access_attr["rule"] not in s_off_set_info.rules:
-                    result.register_issue(
-                        checker_bundle_name=constants.BUNDLE_NAME,
-                        checker_id=CHECKER_ID,
-                        issue_id=issue_count,
-                        description="At a given s-position, either only deny or only allow values shall be given, not mixed.",
-                        level=IssueSeverity.ERROR,
-                    )
-                    path = root.getpath(access)
-                    previous_rule = list(s_off_set_info.rules)[0]
-                    current_rule = access_attr["rule"]
-                    result.add_xml_location(
-                        checker_bundle_name=constants.BUNDLE_NAME,
-                        checker_id=CHECKER_ID,
-                        issue_id=issue_count,
-                        xpath=path,
-                        description=f"First encounter of {current_rule} having {previous_rule} before.",
-                    )
-                    issue_count += 1
+                )
 
     logging.info(f"Issues found - {issue_count}")
 
