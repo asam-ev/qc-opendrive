e0911c2ff7de125f4aea8cc56780dfbe358ea8f0
patrickpa <patrick@ivex.ai>
2024-06-12 09:30:52 +0200

    Add checker status to semantic checker
    Signed-off-by: patrickpa <patrick@ivex.ai>

--- qc_opendrive/checks/semantic/semantic_checker.py
+++ qc_opendrive/checks/semantic/semantic_checker.py
@@ -2,7 +2,7 @@ import logging
 
 from lxml import etree
 
-from qc_baselib import Configuration, Result
+from qc_baselib import Configuration, Result, StatusType
 
 from qc_opendrive import constants
 
@@ -35,4 +35,11 @@ def run_checks(config: Configuration, result: Result) -> None:
 
     logging.info(
         f"Issues found - {result.get_checker_issue_count(checker_bundle_name=constants.BUNDLE_NAME, checker_id=semantic_constants.CHECKER_ID)}"
+    )
+
+    # TODO: Add logic to deal with error or to skip it
+    result.set_checker_status(
+        checker_bundle_name=constants.BUNDLE_NAME,
+        checker_id=semantic_constants.CHECKER_ID,
+        status=StatusType.COMPLETED,
     )
