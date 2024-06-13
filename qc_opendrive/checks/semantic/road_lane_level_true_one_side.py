a9b9e3d4120882d2875dc2d246451ec1b89596db
Patrick Abrahão <patrickabrahao.pa@gmail.com>
2024-06-13 16:14:21 +0200

    Add lane section level check (#17)
    Signed-off-by: patrickpa <patrick@ivex.ai>

--- qc_opendrive/checks/semantic/road_lane_level_true_one_side.py
+++ qc_opendrive/checks/semantic/road_lane_level_true_one_side.py
@@ -1,6 +1,7 @@
 import logging
 
-from typing import Union, List
+from typing import Union, List, Dict, Set
+from enum import Enum
 
 from lxml import etree
 
@@ -59,13 +60,93 @@ def _check_true_level_on_side(
                 )
 
 
-# Check lane level change in between lane sections true to false and false
-# to true
+class LinkageTag(str, Enum):
+    PREDECESSOR = "predecessor"
+    SUCCESSOR = "successor"
+
+
+def _get_lane_level(lanes: List[etree._Element], lane_id: int) -> Union[str, None]:
+    return next(
+        (lane.attrib["level"] for lane in lanes if int(lane.attrib["id"]) == lane_id),
+        None,
+    )
+
+
+def _get_linkage_level_warnings(
+    root: etree._ElementTree,
+    current_lanes: List[etree._Element],
+    target_lanes: List[etree._Element],
+    linkage_tag: LinkageTag,
+):
+    warnings: Set[str] = set()
+
+    for lane in current_lanes:
+        lane_id = int(lane.attrib["id"])
+        lane_level = lane.attrib["level"]
+
+        links: Union[None, List[etree._Element]] = lane.findall("link")
+        for link in links:
+            linkages: List[etree._Element] = link.findall(linkage_tag)
+
+            for linkage in linkages:
+                linkage_id = int(linkage.attrib["id"])
+                linkage_level = _get_lane_level(target_lanes, linkage_id)
+
+                if linkage_level is not None and linkage_level != lane_level:
+                    warnings.add(root.getpath(lane))
+
+    return warnings
+
+
 def _check_level_change_between_lane_sections(
-    current_lane_section: etree._ElementTree, previous_lane_section: etree._ElementTree
+    root: etree._ElementTree,
+    current_lanes: List[etree._Element],
+    previous_lanes: List[etree._Element],
+    result: Result,
+    rule_uid: str,
 ) -> None:
-    # TODO: Implement warning for change in between lane sections
-    return
+    """
+    Check two consecutive lane section from a road if a false level occurs
+    after a true consecutively.
+
+    The predecessor/successor should be pointing to a valid lane id, otherwise
+    it will be ignored.
+
+    If lanes are linked twice by both predecessor and successor, the
+    function will register the issue twice, one for each linkage.
+    """
+
+    predecessor_warnings = _get_linkage_level_warnings(
+        root=root,
+        current_lanes=current_lanes,
+        target_lanes=previous_lanes,
+        linkage_tag=LinkageTag.PREDECESSOR,
+    )
+    successor_warnings = _get_linkage_level_warnings(
+        root=root,
+        current_lanes=previous_lanes,
+        target_lanes=current_lanes,
+        linkage_tag=LinkageTag.SUCCESSOR,
+    )
+
+    warnings = predecessor_warnings | successor_warnings
+    warnings = sorted(list(warnings))
+
+    for warning in warnings:
+        issue_id = result.register_issue(
+            checker_bundle_name=constants.BUNDLE_NAME,
+            checker_id=semantic_constants.CHECKER_ID,
+            description="",
+            level=IssueSeverity.WARNING,
+            rule_uid=rule_uid,
+        )
+        result.add_xml_location(
+            checker_bundle_name=constants.BUNDLE_NAME,
+            checker_id=semantic_constants.CHECKER_ID,
+            issue_id=issue_id,
+            xpath=warning,
+            description="",
+        )
 
 
 def check_rule(checker_data: models.CheckerData) -> None:
@@ -93,57 +174,75 @@ def check_rule(checker_data: models.CheckerData) -> None:
         rule_full_name="road.lane.level.true.one_side",
     )
 
-    lane_sections = utils.get_lane_sections(checker_data.input_file_xml_root)
+    roads = utils.get_roads(checker_data.input_file_xml_root)
 
-    # Sort by s attribute to guarantee order
-    sorted_lane_sections = sorted(
-        lane_sections, key=lambda section: float(section.attrib["s"])
-    )
+    for road in roads:
+        lane_sections = utils.get_lane_sections(road)
 
-    previous_lane_section: Union[etree._ElementTree, None] = None
-
-    for lane_section in sorted_lane_sections:
-        left_lane = lane_section.find("left")
-        if left_lane is not None:
-            left_lanes_list = left_lane.findall("lane")
-        else:
-            left_lanes_list = []
-
-        right_lane = lane_section.find("right")
-        if right_lane is not None:
-            right_lanes_list = right_lane.findall("lane")
-        else:
-            right_lanes_list = []
-
-        # sort by lane id to guarantee order while checking level
-        # right ids goes monotonic increasing from 1
-        sorted_left_lane = sorted(
-            left_lanes_list, key=lambda lane: int(lane.attrib["id"])
+        # Sort by s attribute to guarantee order
+        sorted_lane_sections = sorted(
+            lane_sections, key=lambda section: float(section.attrib["s"])
         )
 
-        _check_true_level_on_side(
-            checker_data.input_file_xml_root,
-            sorted_left_lane,
-            checker_data.result,
-            rule_uid,
-        )
+        previous_left_lane: List[etree._Element] = []
+        previous_right_lane: List[etree._Element] = []
 
-        # sort by lane abs(id) to guarantee order while checking level
-        # right ids goes monotonic decreasing from -1
-        sorted_right_lane = sorted(
-            right_lanes_list, key=lambda lane: abs(int(lane.attrib["id"]))
-        )
+        for lane_section in sorted_lane_sections:
+            left_lane = lane_section.find("left")
+            if left_lane is not None:
+                left_lanes_list = left_lane.findall("lane")
+            else:
+                left_lanes_list = []
 
-        _check_true_level_on_side(
-            checker_data.input_file_xml_root,
-            sorted_right_lane,
-            checker_data.result,
-            rule_uid,
-        )
+            right_lane = lane_section.find("right")
+            if right_lane is not None:
+                right_lanes_list = right_lane.findall("lane")
+            else:
+                right_lanes_list = []
 
-        # check for lane level changing in between consecutive lane sections
-        _check_level_change_between_lane_sections(
-            current_lane_section=lane_section,
-            previous_lane_section=previous_lane_section,
-        )
-        previous_lane_section = lane_section
+            # sort by lane id to guarantee order while checking level
+            # left ids goes monotonic increasing from 1
+            sorted_left_lane = sorted(
+                left_lanes_list, key=lambda lane: int(lane.attrib["id"])
+            )
+
+            _check_true_level_on_side(
+                checker_data.input_file_xml_root,
+                sorted_left_lane,
+                checker_data.result,
+                rule_uid,
+            )
+
+            # sort by lane abs(id) to guarantee order while checking level
+            # right ids goes monotonic decreasing from -1
+            sorted_right_lane = sorted(
+                right_lanes_list, key=lambda lane: abs(int(lane.attrib["id"]))
+            )
+
+            _check_true_level_on_side(
+                checker_data.input_file_xml_root,
+                sorted_right_lane,
+                checker_data.result,
+                rule_uid,
+            )
+
+            # check for lane level changing in between consecutive lane sections
+            _check_level_change_between_lane_sections(
+                root=checker_data.input_file_xml_root,
+                current_lanes=sorted_left_lane,
+                previous_lanes=previous_left_lane,
+                result=checker_data.result,
+                rule_uid=rule_uid,
+            )
+            _check_level_change_between_lane_sections(
+                root=checker_data.input_file_xml_root,
+                current_lanes=sorted_right_lane,
+                previous_lanes=previous_right_lane,
+                result=checker_data.result,
+                rule_uid=rule_uid,
+            )
+
+            previous_left_lane = sorted_left_lane
+            previous_right_lane = sorted_right_lane
+
+    # TODO: Add inter road lane level validation
