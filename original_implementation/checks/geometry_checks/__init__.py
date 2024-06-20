4b0c2cbe12952a0eb559fc484ceb7657808c166a
hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>
2024-06-20 16:49:34 +0200

    Remove the original implementation
    Signed-off-by: hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>

--- original_implementation/checks/geometry_checks/__init__.py
+++
@@ -1,4 +0,0 @@
-CHECKER_BUNDLE_NAME='Geometry'
-CHECKER_BUNDLE_DESCRIPTION='Checks for geometry of ODR file.'
-CHECKER_BUNDLE_VERSION='1.0'
-ORDER=['check_road_min_length', 'check_road_geometry_length', 'check_paramPoly3']
