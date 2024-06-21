a1867b6085390f69a0938d5d27198895e5b2e6a8
hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>
2024-06-21 09:17:43 +0200

    Add check for inter-road lane level
    Signed-off-by: hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>

--- qc_opendrive/checks/semantic/road_lane_level_true_one_side.py
+++ qc_opendrive/checks/semantic/road_lane_level_true_one_side.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import logging
 
 from typing import Union, List, Dict, Set
@@ -33,7 +34,6 @@ def _check_true_level_on_side(
         lane_attrib = lane.attrib
 
         if "level" in lane_attrib:
-
             lane_level = utils.xml_string_to_bool(lane_attrib["level"])
 
             if lane_level == True:
@@ -60,39 +60,32 @@ def _check_true_level_on_side(
                 )
 
 
-class LinkageTag(str, Enum):
-    PREDECESSOR = "predecessor"
-    SUCCESSOR = "successor"
-
-
-def _get_lane_level(lanes: List[etree._Element], lane_id: int) -> Union[str, None]:
-    return next(
-        (lane.attrib["level"] for lane in lanes if int(lane.attrib["id"]) == lane_id),
-        None,
-    )
-
-
 def _get_linkage_level_warnings(
     root: etree._ElementTree,
-    current_lanes: List[etree._Element],
-    target_lanes: List[etree._Element],
-    linkage_tag: LinkageTag,
+    current_lane_section: etree._ElementTree,
+    target_lane_section: etree._ElementTree,
+    linkage_tag: models.LinkageTag,
 ):
     warnings: Set[str] = set()
 
-    for lane in current_lanes:
-        lane_id = int(lane.attrib["id"])
-        lane_level = lane.attrib["level"]
+    for lane in utils.get_left_and_right_lanes_from_lane_section(current_lane_section):
+        lane_level = utils.get_lane_level_from_lane(lane)
 
-        links: Union[None, List[etree._Element]] = lane.findall("link")
-        for link in links:
-            linkages: List[etree._Element] = link.findall(linkage_tag)
+        for link in lane.findall("link"):
+            for linkage in link.findall(linkage_tag):
+                linkage_id = linkage.get("id")
+                if linkage_id is None:
+                    continue
+                linkage_id = int(linkage_id)
+                linkage_lane = utils.get_lane_from_lane_section(
+                    target_lane_section, linkage_id
+                )
+                if linkage_lane is None:
+                    continue
 
-            for linkage in linkages:
-                linkage_id = int(linkage.attrib["id"])
-                linkage_level = _get_lane_level(target_lanes, linkage_id)
+                linkage_level = utils.get_lane_level_from_lane(linkage_lane)
 
-                if linkage_level is not None and linkage_level != lane_level:
+                if linkage_level != lane_level:
                     warnings.add(root.getpath(lane))
 
     return warnings
@@ -100,8 +93,8 @@ def _get_linkage_level_warnings(
 
 def _check_level_change_between_lane_sections(
     root: etree._ElementTree,
-    current_lanes: List[etree._Element],
-    previous_lanes: List[etree._Element],
+    current_lane_section: etree._ElementTree,
+    previous_lane_section: etree._ElementTree,
     result: Result,
     rule_uid: str,
 ) -> None:
@@ -118,15 +111,15 @@ def _check_level_change_between_lane_sections(
 
     predecessor_warnings = _get_linkage_level_warnings(
         root=root,
-        current_lanes=current_lanes,
-        target_lanes=previous_lanes,
-        linkage_tag=LinkageTag.PREDECESSOR,
+        current_lane_section=current_lane_section,
+        target_lane_section=previous_lane_section,
+        linkage_tag=models.LinkageTag.PREDECESSOR,
     )
     successor_warnings = _get_linkage_level_warnings(
         root=root,
-        current_lanes=previous_lanes,
-        target_lanes=current_lanes,
-        linkage_tag=LinkageTag.SUCCESSOR,
+        current_lane_section=previous_lane_section,
+        target_lane_section=current_lane_section,
+        linkage_tag=models.LinkageTag.SUCCESSOR,
     )
 
     warnings = predecessor_warnings | successor_warnings
@@ -136,7 +129,7 @@ def _check_level_change_between_lane_sections(
         issue_id = result.register_issue(
             checker_bundle_name=constants.BUNDLE_NAME,
             checker_id=semantic_constants.CHECKER_ID,
-            description="",
+            description="Lane levels are not the same in two consecutive lane sections",
             level=IssueSeverity.WARNING,
             rule_uid=rule_uid,
         )
@@ -149,56 +142,125 @@ def _check_level_change_between_lane_sections(
         )
 
 
-def check_rule(checker_data: models.CheckerData) -> None:
-    """
-    Implements a rule to check if there is any @Level=False after true until
-    the lane border.
+def _check_level_change_linkage_roads(
+    linkage_tag: models.LinkageTag,
+    road: etree._ElementTree,
+    road_id_map: Dict[int, etree._ElementTree],
+    result: Result,
+    rule_uid: str,
+    root: etree._ElementTree,
+) -> None:
+    if linkage_tag == models.LinkageTag.PREDECESSOR:
+        current_lane_section = utils.get_first_lane_section(road)
+    else:
+        current_lane_section = utils.get_last_lane_section(road)
 
-    More info at
-        - https://github.com/asam-ev/qc-opendrive/issues/2
-    """
-    logging.info("Executing road.lane.level.true.one_side check")
+    all_lanes = utils.get_left_and_right_lanes_from_lane_section(current_lane_section)
 
-    if checker_data.schema_version < RULE_INITIAL_SUPPORTED_SCHEMA_VERSION:
-        logging.info(
-            f"Schema version {checker_data.schema_version} not supported. Skipping rule."
-        )
+    linkage = utils.get_road_linkage(road, linkage_tag)
+    if linkage is None:
         return
 
-    rule_uid = checker_data.result.register_rule(
-        checker_bundle_name=constants.BUNDLE_NAME,
-        checker_id=semantic_constants.CHECKER_ID,
-        emanating_entity="asam.net",
-        standard="xodr",
-        definition_setting=RULE_INITIAL_SUPPORTED_SCHEMA_VERSION,
-        rule_full_name="road.lane.level.true.one_side",
-    )
+    linkage_road = road_id_map.get(linkage.id)
+    if linkage_road is None:
+        return
 
+    other_lane_section = None
+    if linkage.contact_point == models.ContactPoint.START:
+        other_lane_section = utils.get_first_lane_section(linkage_road)
+    elif linkage.contact_point == models.ContactPoint.END:
+        other_lane_section = utils.get_last_lane_section(linkage_road)
+
+    if other_lane_section is None:
+        return
+
+    for lane in all_lanes:
+        lane_level = utils.get_lane_level_from_lane(lane)
+
+        if linkage_tag == models.LinkageTag.PREDECESSOR:
+            linkage_lane_ids = utils.get_predecessor_lane_ids(lane)
+        else:
+            linkage_lane_ids = utils.get_successor_lane_ids(lane)
+
+        for lane_id in linkage_lane_ids:
+            other_lane = utils.get_lane_from_lane_section(other_lane_section, lane_id)
+            if other_lane is None:
+                continue
+
+            other_lane_level = utils.get_lane_level_from_lane(other_lane)
+            if other_lane_level is not None and other_lane_level != lane_level:
+                issue_id = result.register_issue(
+                    checker_bundle_name=constants.BUNDLE_NAME,
+                    checker_id=semantic_constants.CHECKER_ID,
+                    description="Lane levels are not the same between two connected roads.",
+                    level=IssueSeverity.WARNING,
+                    rule_uid=rule_uid,
+                )
+
+                result.add_xml_location(
+                    checker_bundle_name=constants.BUNDLE_NAME,
+                    checker_id=semantic_constants.CHECKER_ID,
+                    issue_id=issue_id,
+                    xpath=root.getpath(lane),
+                    description="",
+                )
+
+
+def _check_level_among_lane_sections(
+    checker_data: models.CheckerData,
+    rule_uid: str,
+) -> None:
     roads = utils.get_roads(checker_data.input_file_xml_root)
+    for road in roads:
+        lane_sections = utils.get_lane_sections(road)
+        if len(lane_sections) >= 2:
+            # check for lane level changing in between consecutive lane sections
+            for i in range(1, len(lane_sections)):
+                _check_level_change_between_lane_sections(
+                    root=checker_data.input_file_xml_root,
+                    current_lane_section=lane_sections[i],
+                    previous_lane_section=lane_sections[i - 1],
+                    result=checker_data.result,
+                    rule_uid=rule_uid,
+                )
 
+
+def _check_level_among_roads(
+    checker_data: models.CheckerData,
+    road_id_map: Dict[int, etree._ElementTree],
+    rule_uid: str,
+) -> None:
+    roads = utils.get_roads(checker_data.input_file_xml_root)
+    for road in roads:
+        _check_level_change_linkage_roads(
+            linkage_tag=models.LinkageTag.PREDECESSOR,
+            road=road,
+            road_id_map=road_id_map,
+            result=checker_data.result,
+            rule_uid=rule_uid,
+            root=checker_data.input_file_xml_root,
+        )
+        _check_level_change_linkage_roads(
+            linkage_tag=models.LinkageTag.SUCCESSOR,
+            road=road,
+            road_id_map=road_id_map,
+            result=checker_data.result,
+            rule_uid=rule_uid,
+            root=checker_data.input_file_xml_root,
+        )
+
+
+def _check_level_in_lane_section(
+    checker_data: models.CheckerData,
+    rule_uid: str,
+) -> None:
+    roads = utils.get_roads(checker_data.input_file_xml_root)
     for road in roads:
         lane_sections = utils.get_lane_sections(road)
 
-        # Sort by s attribute to guarantee order
-        sorted_lane_sections = sorted(
-            lane_sections, key=lambda section: float(section.attrib["s"])
-        )
-
-        previous_left_lane: List[etree._Element] = []
-        previous_right_lane: List[etree._Element] = []
-
-        for lane_section in sorted_lane_sections:
-            left_lane = lane_section.find("left")
-            if left_lane is not None:
-                left_lanes_list = left_lane.findall("lane")
-            else:
-                left_lanes_list = []
-
-            right_lane = lane_section.find("right")
-            if right_lane is not None:
-                right_lanes_list = right_lane.findall("lane")
-            else:
-                right_lanes_list = []
+        for lane_section in lane_sections:
+            left_lanes_list = utils.get_left_lanes_from_lane_section(lane_section)
+            right_lanes_list = utils.get_right_lanes_from_lane_section(lane_section)
 
             # sort by lane id to guarantee order while checking level
             # left ids goes monotonic increasing from 1
@@ -226,23 +288,123 @@ def check_rule(checker_data: models.CheckerData) -> None:
                 rule_uid,
             )
 
-            # check for lane level changing in between consecutive lane sections
-            _check_level_change_between_lane_sections(
-                root=checker_data.input_file_xml_root,
-                current_lanes=sorted_left_lane,
-                previous_lanes=previous_left_lane,
-                result=checker_data.result,
-                rule_uid=rule_uid,
-            )
-            _check_level_change_between_lane_sections(
-                root=checker_data.input_file_xml_root,
-                current_lanes=sorted_right_lane,
-                previous_lanes=previous_right_lane,
-                result=checker_data.result,
-                rule_uid=rule_uid,
-            )
 
-            previous_left_lane = sorted_left_lane
-            previous_right_lane = sorted_right_lane
+def _check_level_among_junctions(
+    checker_data: models.CheckerData,
+    road_id_map: Dict[int, etree._ElementTree],
+    rule_uid: str,
+) -> None:
+    for junction in checker_data.input_file_xml_root.iter("junction"):
+        for connection in junction.iter("connection"):
+            connection_road_id = connection.get("connectingRoad")
+            incoming_road_id = connection.get("incomingRoad")
+            connection_road = road_id_map.get(int(connection_road_id))
+            incoming_road = road_id_map.get(int(incoming_road_id))
 
-    # TODO: Add inter road lane level validation
+            if connection_road is None or incoming_road is None:
+                continue
+
+            connection_lane_sections = utils.get_lane_sections(connection_road)
+            incoming_lane_sections = utils.get_lane_sections(incoming_road)
+
+            if len(connection_lane_sections) == 0 or len(incoming_lane_sections) == 0:
+                continue
+
+            contact_point = connection.get("contactPoint")
+            incoming_lane_section = None
+            connection_lane_section = None
+            if contact_point == "start":
+                incoming_lane_section = incoming_lane_sections[-1]
+                connection_lane_section = connection_lane_sections[0]
+            elif contact_point == "end":
+                # Case outgoing lane
+                incoming_lane_section = incoming_lane_sections[0]
+                connection_lane_section = connection_lane_sections[-1]
+            else:
+                continue
+
+            for lane_link in connection.iter("laneLink"):
+                incoming_lane_id = lane_link.get("from")
+                connection_lane_id = lane_link.get("to")
+
+                if incoming_lane_id is None or connection_lane_id is None:
+                    continue
+
+                incoming_lane = utils.get_lane_from_lane_section(
+                    incoming_lane_section, int(incoming_lane_id)
+                )
+                connection_lane = utils.get_lane_from_lane_section(
+                    connection_lane_section, int(connection_lane_id)
+                )
+
+                if incoming_lane is None or connection_lane is None:
+                    continue
+
+                incoming_level = utils.get_lane_level_from_lane(incoming_lane)
+                connection_level = utils.get_lane_level_from_lane(connection_lane)
+
+                if incoming_level != connection_level:
+                    issue_id = checker_data.result.register_issue(
+                        checker_bundle_name=constants.BUNDLE_NAME,
+                        checker_id=semantic_constants.CHECKER_ID,
+                        description="Lane levels are not the same between incoming road and junction.",
+                        level=IssueSeverity.WARNING,
+                        rule_uid=rule_uid,
+                    )
+
+                    checker_data.result.add_xml_location(
+                        checker_bundle_name=constants.BUNDLE_NAME,
+                        checker_id=semantic_constants.CHECKER_ID,
+                        issue_id=issue_id,
+                        xpath=checker_data.input_file_xml_root.getpath(incoming_lane),
+                        description="",
+                    )
+
+                    issue_id = checker_data.result.register_issue(
+                        checker_bundle_name=constants.BUNDLE_NAME,
+                        checker_id=semantic_constants.CHECKER_ID,
+                        description="Lane levels are not the same between junction and incoming road.",
+                        level=IssueSeverity.WARNING,
+                        rule_uid=rule_uid,
+                    )
+
+                    checker_data.result.add_xml_location(
+                        checker_bundle_name=constants.BUNDLE_NAME,
+                        checker_id=semantic_constants.CHECKER_ID,
+                        issue_id=issue_id,
+                        xpath=checker_data.input_file_xml_root.getpath(connection_lane),
+                        description="",
+                    )
+
+
+def check_rule(checker_data: models.CheckerData) -> None:
+    """
+    Implements a rule to check if there is any @Level=False after true until
+    the lane border.
+
+    More info at
+        - https://github.com/asam-ev/qc-opendrive/issues/2
+    """
+    logging.info("Executing road.lane.level.true.one_side check")
+
+    rule_uid = checker_data.result.register_rule(
+        checker_bundle_name=constants.BUNDLE_NAME,
+        checker_id=semantic_constants.CHECKER_ID,
+        emanating_entity="asam.net",
+        standard="xodr",
+        definition_setting=RULE_INITIAL_SUPPORTED_SCHEMA_VERSION,
+        rule_full_name="road.lane.level_true_one_side",
+    )
+
+    if checker_data.schema_version < RULE_INITIAL_SUPPORTED_SCHEMA_VERSION:
+        logging.info(
+            f"Schema version {checker_data.schema_version} not supported. Skipping rule."
+        )
+        return
+
+    road_id_map = utils.get_road_id_map(checker_data.input_file_xml_root)
+
+    _check_level_in_lane_section(checker_data, rule_uid)
+    _check_level_among_lane_sections(checker_data, rule_uid)
+    _check_level_among_roads(checker_data, road_id_map, rule_uid)
+    _check_level_among_junctions(checker_data, road_id_map, rule_uid)
--- qc_opendrive/checks/utils.py
+++ qc_opendrive/checks/utils.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Dict, Union
 
 from lxml import etree
+from qc_opendrive.checks import models
 
 
 def get_lanes(root: etree._ElementTree) -> List[etree._ElementTree]:
@@ -12,13 +13,29 @@ def get_lanes(root: etree._ElementTree) -> List[etree._ElementTree]:
     return lanes
 
 
-def get_lane_sections(root: etree._ElementTree) -> List[etree._ElementTree]:
+def get_lane_sections(road: etree._ElementTree) -> List[etree._ElementTree]:
     lane_sections = []
 
-    for lane_section in root.iter("laneSection"):
+    for lane_section in road.iter("laneSection"):
         lane_sections.append(lane_section)
 
     return lane_sections
+
+
+def get_last_lane_section(road: etree._ElementTree) -> Union[None, etree._ElementTree]:
+    lane_sections = get_lane_sections(road)
+    if len(lane_sections) > 0:
+        return lane_sections[-1]
+    else:
+        return None
+
+
+def get_first_lane_section(road: etree._ElementTree) -> Union[None, etree._ElementTree]:
+    lane_sections = get_lane_sections(road)
+    if len(lane_sections) > 0:
+        return lane_sections[0]
+    else:
+        return None
 
 
 def get_roads(root: etree._ElementTree) -> List[etree._ElementTree]:
@@ -30,6 +47,73 @@ def get_roads(root: etree._ElementTree) -> List[etree._ElementTree]:
     return roads
 
 
+def get_road_id_map(root: etree._ElementTree) -> Dict[int, etree._ElementTree]:
+    """
+    Returns a dictionary where keys are the road IDs and values are the road.
+    Roads without a valid ID are not included in the dictionary.
+    If there are multiple roads with the same ID, a random road will be included in the dictionary
+    """
+
+    road_id_map = dict()
+
+    for road in root.iter("road"):
+        road_id = road.get("id")
+        if road_id is not None:
+            road_id_map[int(road_id)] = road
+
+    return road_id_map
+
+
+def get_junction_id_map(root: etree._ElementTree) -> Dict[int, etree._ElementTree]:
+    """
+    Returns a dictionary where keys are the junction IDs and values are the junction.
+    Junctions without a valid ID are not included in the dictionary.
+    If there are multiple junctions with the same ID, a random junction will be included in the dictionary
+    """
+
+    junction_id_map = dict()
+
+    for junction in root.iter("junction"):
+        junction_id = junction.get("id")
+        if junction_id is not None:
+            junction_id_map[int(junction_id)] = junction
+
+    return junction_id_map
+
+
+def get_left_lanes_from_lane_section(
+    lane_section: etree._ElementTree,
+) -> List[etree._Element]:
+    left_lane = lane_section.find("left")
+    if left_lane is not None:
+        left_lanes_list = left_lane.findall("lane")
+    else:
+        left_lanes_list = []
+
+    return left_lanes_list
+
+
+def get_right_lanes_from_lane_section(
+    lane_section: etree._ElementTree,
+) -> List[etree._Element]:
+    right_lane = lane_section.find("right")
+    if right_lane is not None:
+        right_lanes_list = right_lane.findall("lane")
+    else:
+        right_lanes_list = []
+
+    return right_lanes_list
+
+
+def get_left_and_right_lanes_from_lane_section(
+    lane_section: etree._ElementTree,
+) -> List[etree._Element]:
+    left_lanes = get_left_lanes_from_lane_section(lane_section)
+    right_lanes = get_right_lanes_from_lane_section(lane_section)
+
+    return left_lanes + right_lanes
+
+
 def xml_string_to_bool(value: str):
     return value.lower() in ("true",)
 
@@ -39,3 +123,72 @@ def get_standard_schema_version(root: etree._ElementTree) -> str:
     header_attrib = header.attrib
     version = f"{header_attrib['revMajor']}.{header_attrib['revMinor']}.0"
     return version
+
+
+def get_road_linkage(
+    road: etree._ElementTree, linkage_tag: models.LinkageTag
+) -> Union[None, models.RoadLinkage]:
+    road_link = road.find("link")
+    if road_link is None:
+        return None
+
+    linkage = road_link.find(linkage_tag.value)
+
+    if linkage is None:
+        return None
+    elif linkage.get("elementType") == "road":
+        road_id = linkage.get("elementId")
+        contact_point = linkage.get("contactPoint")
+        if road_id is None or contact_point is None:
+            return None
+        else:
+            return models.RoadLinkage(
+                id=int(road_id), contact_point=models.ContactPoint(contact_point)
+            )
+    else:
+        return None
+
+
+def get_predecessor_lane_ids(lane: etree._ElementTree) -> List[int]:
+    links = lane.findall("link")
+
+    predecessors = []
+    for link in links:
+        linkages = link.findall("predecessor")
+        for linkage in linkages:
+            predecessor_id = linkage.get("id")
+            if predecessor_id is not None:
+                predecessors.append(int(predecessor_id))
+
+    return predecessors
+
+
+def get_successor_lane_ids(lane: etree._ElementTree) -> List[int]:
+    links = lane.findall("link")
+
+    successors = []
+    for link in links:
+        linkages = link.findall("successor")
+        for linkage in linkages:
+            successor_id = linkage.get("id")
+            if successor_id is not None:
+                successors.append(int(successor_id))
+
+    return successors
+
+
+def get_lane_from_lane_section(
+    lane_section: etree._ElementTree, lane_id: int
+) -> Union[None, etree._ElementTree]:
+    lanes = get_left_and_right_lanes_from_lane_section(lane_section)
+
+    for lane in lanes:
+        current_id = lane.get("id")
+        if current_id is not None and int(current_id) == lane_id:
+            return lane
+
+    return None
+
+
+def get_lane_level_from_lane(lane: etree._ElementTree) -> bool:
+    return lane.get("level") == "true"
