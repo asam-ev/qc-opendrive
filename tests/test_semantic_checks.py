3e391b2ccfc5e333d5df54bb1aab7e93fa97b3cf
patrickpa <patrick@ivex.ai>
2024-06-27 10:06:44 +0200

    Add tests for the base cases
    Signed-off-by: patrickpa <patrick@ivex.ai>

--- tests/test_semantic_checks.py
+++ tests/test_semantic_checks.py
@@ -364,3 +364,38 @@ def test_road_linkage_is_junction_needed(
     launch_main(monkeypatch)
     check_issues(rule_uid, issue_count, issue_xpath, issue_severity)
     cleanup_files()
+
+
+#
+@pytest.mark.parametrize(
+    "target_file,issue_count,issue_xpath",
+    [
+        ("valid", 0, []),
+        (
+            "invalid",
+            1,
+            [
+                "/OpenDRIVE/junction[2]/connection[1]",
+                "/OpenDRIVE/junction[2]/connection[2]",
+            ],
+        ),
+    ],
+)
+def test_junctions_connection_road_no_incoming_road(
+    target_file: str,
+    issue_count: int,
+    issue_xpath: List[str],
+    monkeypatch,
+) -> None:
+    base_path = "tests/data/junctions_connection_connect_road_no_incoming_road/"
+    target_file_name = (
+        f"junctions_connection_connect_road_no_incoming_road_{target_file}.xodr"
+    )
+    rule_uid = "asam.net:xodr:1.4.0:junctions.connection.connect_road_no_incoming_road"
+    issue_severity = IssueSeverity.ERROR
+
+    target_file_path = os.path.join(base_path, target_file_name)
+    create_test_config(target_file_path)
+    launch_main(monkeypatch)
+    check_issues(rule_uid, issue_count, issue_xpath, issue_severity)
+    cleanup_files()
