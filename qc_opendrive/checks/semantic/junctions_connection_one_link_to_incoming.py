d6837f3af56aab324f03176da395c79c92240912
patrickpa <patrick@ivex.ai>
2024-06-28 10:36:06 +0200

    Add initial implementation for the rule
    Signed-off-by: patrickpa <patrick@ivex.ai>

---
+++ qc_opendrive/checks/semantic/junctions_connection_one_link_to_incoming.py
@@ -0,0 +1,172 @@
+import logging
+
+from typing import Dict, List
+from lxml import etree
+
+from qc_baselib import IssueSeverity
+
+from qc_opendrive import constants
+from qc_opendrive.checks import utils, models
+from qc_opendrive.checks.semantic import semantic_constants
+
+RULE_INITIAL_SUPPORTED_SCHEMA_VERSION = "1.8.0"
+
+
+def _check_connection_lane_link_same_direction(
+    checker_data: models.CheckerData,
+    road_id_map: Dict[int, etree._ElementTree],
+    connection: etree._Element,
+    rule_uid: str,
+) -> None:
+    contact_point = utils.get_contact_point_from_connection(connection)
+
+    # if not explicitly added we assume start as default
+    if contact_point is None:
+        contact_point = models.ContactPoint.START
+
+    incoming_road_id = utils.get_incoming_road_id_from_connection(connection)
+    connecting_road_id = utils.get_connecting_road_id_from_connection(connection)
+
+    incoming_road_last_lane_section = utils.get_last_lane_section(
+        road_id_map[incoming_road_id]
+    )
+    connecting_road_id_first_lane_section = utils.get_first_lane_section(
+        road_id_map[connecting_road_id]
+    )
+
+    lane_links = utils.get_lane_links_from_connection(connection)
+
+    for lane_link in lane_links:
+        from_lane_id = utils.get_from_attribute_from_lane_link(lane_link)
+        to_lane_id = utils.get_to_attribute_from_lane_link(lane_link)
+
+        from_lane = None
+        to_lane = None
+        if contact_point == models.ContactPoint.START:
+            from_lane = utils.get_lane_from_lane_section(
+                incoming_road_last_lane_section, from_lane_id
+            )
+            to_lane = utils.get_lane_from_lane_section(
+                connecting_road_id_first_lane_section, to_lane_id
+            )
+
+        else:
+            from_lane = utils.get_lane_from_lane_section(
+                connecting_road_id_first_lane_section, from_lane_id
+            )
+            to_lane = utils.get_lane_from_lane_section(
+                incoming_road_last_lane_section, to_lane_id
+            )
+
+        if from_lane is None or to_lane is None:
+            # raise one issue if a lane link is found in the opposite direction
+            # of the connecting road
+            issue_id = checker_data.result.register_issue(
+                checker_bundle_name=constants.BUNDLE_NAME,
+                checker_id=semantic_constants.CHECKER_ID,
+                description=f"A connecting road shall only have the <laneLink> element for that direction.",
+                level=IssueSeverity.ERROR,
+                rule_uid=rule_uid,
+            )
+            checker_data.result.add_xml_location(
+                checker_bundle_name=constants.BUNDLE_NAME,
+                checker_id=semantic_constants.CHECKER_ID,
+                issue_id=issue_id,
+                xpath=checker_data.input_file_xml_root.getpath(lane_link),
+                description=f"Lane link in opposite direction.",
+            )
+
+
+def _check_junctions_connection_one_link_to_incoming(
+    checker_data: models.CheckerData, rule_uid: str
+) -> None:
+    junctions = utils.get_junctions(checker_data.input_file_xml_root)
+    road_id_map = utils.get_road_id_map(checker_data.input_file_xml_root)
+
+    connection_road_link_map: Dict[int, Dict[int, List[etree._Element]]] = {}
+
+    for junction in junctions:
+        connections = utils.get_connections_from_junction(junction)
+
+        for connection in connections:
+            incoming_road_id = utils.get_incoming_road_id_from_connection(connection)
+            connecting_road_id = utils.get_connecting_road_id_from_connection(
+                connection
+            )
+
+            if incoming_road_id not in connection_road_link_map:
+                connection_road_link_map[incoming_road_id] = {}
+            if connecting_road_id not in connection_road_link_map[incoming_road_id]:
+                connection_road_link_map[incoming_road_id][connecting_road_id] = []
+
+            connection_road_link_map[incoming_road_id][connecting_road_id].append(
+                connection
+            )
+
+            _check_connection_lane_link_same_direction(
+                checker_data, road_id_map, connection, rule_uid
+            )
+
+    for incoming_road_id, connecting_road_map in connection_road_link_map.items():
+        for connecting_road_id, connections in connecting_road_map.items():
+            if len(connections) > 1:
+                # raise one issue if the pair (incoming_road_id, connecting_road_id)
+                # appears in more than one connection.
+                issue_id = checker_data.result.register_issue(
+                    checker_bundle_name=constants.BUNDLE_NAME,
+                    checker_id=semantic_constants.CHECKER_ID,
+                    description=f"Connecting road {connecting_road_id} shall be represented by at most one <connection> element per incoming road id.",
+                    level=IssueSeverity.ERROR,
+                    rule_uid=rule_uid,
+                )
+                for connection in connections:
+                    checker_data.result.add_xml_location(
+                        checker_bundle_name=constants.BUNDLE_NAME,
+                        checker_id=semantic_constants.CHECKER_ID,
+                        issue_id=issue_id,
+                        xpath=checker_data.input_file_xml_root.getpath(connection),
+                        description=f"Connection with reused (incoming_road_id, connecting_road_id) = ({incoming_road_id}, {connecting_road_id}) pair.",
+                    )
+
+    return
+
+
+def check_rule(checker_data: models.CheckerData) -> None:
+    """
+    Implements a rule to check if a junction connecting roads are linked to
+    the same incoming road more than once, and if their lane links follow the
+    proper direction.
+
+    More info at
+        - https://github.com/asam-ev/qc-opendrive/issues/10
+
+    Rule ID: junctions.connection.one_link_to_incoming
+
+    Description: Each connecting road shall be associated with at most one
+    <connection> element per incoming road. A connecting road shall only have
+    the <laneLink> element for that direction.
+
+    Severity: ERROR
+
+    Version range: From 1.8.0
+
+    Rule Version: 0.1
+    """
+    logging.info("Executing junctions.connection.one_link_to_incoming check")
+
+    rule_uid = checker_data.result.register_rule(
+        checker_bundle_name=constants.BUNDLE_NAME,
+        checker_id=semantic_constants.CHECKER_ID,
+        emanating_entity="asam.net",
+        standard="xodr",
+        definition_setting=RULE_INITIAL_SUPPORTED_SCHEMA_VERSION,
+        rule_full_name="junctions.connection.one_link_to_incoming",
+    )
+
+    if checker_data.schema_version < RULE_INITIAL_SUPPORTED_SCHEMA_VERSION:
+        logging.info(
+            f"Schema version {checker_data.schema_version} not supported. Skipping rule."
+        )
+        return
+
+    _check_junctions_connection_one_link_to_incoming(checker_data, rule_uid)
