9a54043a49d59828303373a9003c16b193407798
Patrick Abrahão <patrickabrahao.pa@gmail.com>
2024-08-19 12:05:17 +0200

    Ignore namespace on XML file parsing (#64)
    Signed-off-by: patrickpa <patrick@ivex.ai>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,28 @@ from lxml import etree
 from qc_opendrive.base import utils
 
 
+def test_get_root_without_default_namespace() -> None:
+    # file containing namespace
+    root = utils.get_root_without_default_namespace("tests/data/utils/namespace.xodr")
+    assert type(root) == etree._ElementTree
+    # file does not contain namespace
+    root = utils.get_root_without_default_namespace(
+        "tests/data/utils/Ex_Bidirectional_Junction.xodr"
+    )
+    assert type(root) == etree._ElementTree
+
+
 def test_get_road_id_map() -> None:
-    root = etree.parse("tests/data/utils/Ex_Bidirectional_Junction.xodr")
+    root = utils.get_root_without_default_namespace(
+        "tests/data/utils/Ex_Bidirectional_Junction.xodr"
+    )
     road_id_map = utils.get_road_id_map(root)
     assert len(road_id_map) == 6
 
 
 def test_get_junction_id_map() -> None:
-    root = etree.parse("tests/data/utils/Ex_Bidirectional_Junction.xodr")
+    root = utils.get_root_without_default_namespace(
+        "tests/data/utils/Ex_Bidirectional_Junction.xodr"
+    )
     junction_id_map = utils.get_junction_id_map(root)
     assert len(junction_id_map) == 1
