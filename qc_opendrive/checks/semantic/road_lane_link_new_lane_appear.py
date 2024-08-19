916e3385597162ee740612bd10c47fcad45319f5
Patrick Abrahão <patrickabrahao.pa@gmail.com>
2024-08-19 12:00:52 +0200

    Fix appearing lane logic to take into account road contact point (#63)
    Signed-off-by: Patrick Abrahão <patrick@ivex.ai>
    Signed-off-by: Patrick Abrahão <patrickabrahao.pa@gmail.com>

--- qc_opendrive/checks/semantic/road_lane_link_new_lane_appear.py
+++ qc_opendrive/checks/semantic/road_lane_link_new_lane_appear.py
@@ -51,6 +51,8 @@ def _check_successor_with_width_zero_between_lane_sections(
     rule_uid: str,
     current_lane_section: etree._ElementTree,
     next_lane_section: etree._ElementTree,
+    contact_point: models.ContactPoint,
+    next_lane_section_length: float,
 ) -> None:
     current_lanes = utils.get_left_and_right_lanes_from_lane_section(
         current_lane_section
@@ -71,11 +73,18 @@ def _check_successor_with_width_zero_between_lane_sections(
             if successor_lane is None:
                 continue
 
-            start_lane_width = utils.evaluate_lane_width(successor_lane, 0.0)
+            target_lane_width = None
+
+            if contact_point == models.ContactPoint.START:
+                target_lane_width = utils.evaluate_lane_width(successor_lane, 0.0)
+            elif contact_point == models.ContactPoint.END:
+                target_lane_width = utils.evaluate_lane_width(
+                    successor_lane, next_lane_section_length
+                )
 
             if (
-                start_lane_width is not None
-                and abs(start_lane_width) < FLOAT_COMPARISON_THRESHOLD
+                target_lane_width is not None
+                and abs(target_lane_width) < FLOAT_COMPARISON_THRESHOLD
             ):
                 _raise_issue(
                     checker_data,
@@ -86,19 +95,74 @@ def _check_successor_with_width_zero_between_lane_sections(
                 )
 
 
+def _check_predecessor_with_width_zero_between_lane_sections(
+    checker_data: models.CheckerData,
+    rule_uid: str,
+    current_lane_section: etree._ElementTree,
+    next_lane_section: etree._ElementTree,
+    contact_point: models.ContactPoint,
+    next_lane_section_length: float,
+) -> None:
+    current_lanes = utils.get_left_and_right_lanes_from_lane_section(
+        current_lane_section
+    )
+
+    for lane in current_lanes:
+        lane_id = utils.get_lane_id(lane)
+
+        if lane_id is None:
+            continue
+
+        predecessor_lane_ids = utils.get_predecessor_lane_ids(lane)
+
+        for predecessor_lane_id in predecessor_lane_ids:
+            predecessor_lane = utils.get_lane_from_lane_section(
+                next_lane_section, predecessor_lane_id
+            )
+            if predecessor_lane is None:
+                continue
+
+            target_lane_width = None
+
+            if contact_point == models.ContactPoint.START:
+                target_lane_width = utils.evaluate_lane_width(predecessor_lane, 0.0)
+            elif contact_point == models.ContactPoint.END:
+                target_lane_width = utils.evaluate_lane_width(
+                    predecessor_lane, next_lane_section_length
+                )
+
+            if (
+                target_lane_width is not None
+                and abs(target_lane_width) < FLOAT_COMPARISON_THRESHOLD
+            ):
+                _raise_issue(
+                    checker_data,
+                    rule_uid,
+                    lane,
+                    predecessor_lane,
+                    IssueSeverity.ERROR,
+                )
+
+
 def _check_appearing_successor_with_width_zero_on_road(
     checker_data: models.CheckerData, rule_uid: str, road: etree._ElementTree
 ) -> None:
-    lane_sections = utils.get_lane_sections(road)
+    lane_sections = utils.get_sorted_lane_sections_with_length_from_road(road)
 
     if len(lane_sections) < 2:
         return
 
     for index in range(len(lane_sections) - 1):
-        current_lane_section = lane_sections[index]
-        next_lane_section = lane_sections[index + 1]
+        current_lane_section = lane_sections[index].lane_section
+        next_lane_section = lane_sections[index + 1].lane_section
+
         _check_successor_with_width_zero_between_lane_sections(
-            checker_data, rule_uid, current_lane_section, next_lane_section
+            checker_data,
+            rule_uid,
+            current_lane_section,
+            next_lane_section,
+            models.ContactPoint.START,
+            lane_sections[index + 1].length,
         )
 
 
@@ -132,11 +196,71 @@ def _check_appearing_successor_road(
     if successor_road_target_lane_section is None:
         return
 
+    next_lane_section_length = 0.0
+    lane_sections = utils.get_sorted_lane_sections_with_length_from_road(successor_road)
+    if successor_linkage.contact_point == models.ContactPoint.START:
+        next_lane_section_length = lane_sections[0].length
+    elif successor_linkage.contact_point == models.ContactPoint.END:
+        next_lane_section_length = lane_sections[len(lane_sections) - 1].length
+
     _check_successor_with_width_zero_between_lane_sections(
         checker_data,
         rule_uid,
         current_road_last_lane_section,
         successor_road_target_lane_section.lane_section,
+        successor_linkage.contact_point,
+        next_lane_section_length,
+    )
+
+
+def _check_appearing_predecessor_road(
+    checker_data: models.CheckerData,
+    rule_uid: str,
+    road_id_map: Dict[int, etree._ElementTree],
+    current_road_id: int,
+    predecessor_road_id: int,
+) -> None:
+    current_road = road_id_map.get(current_road_id)
+
+    if current_road is None:
+        return
+
+    predecessor_road = road_id_map.get(predecessor_road_id)
+
+    if current_road is None or predecessor_road is None:
+        return
+
+    current_road_last_lane_section = utils.get_last_lane_section(current_road)
+
+    predecessor_linkage = utils.get_road_linkage(
+        current_road, models.LinkageTag.PREDECESSOR
+    )
+
+    predecessor_road_target_lane_section = (
+        utils.get_contact_lane_section_from_linked_road(
+            predecessor_linkage, road_id_map
+        )
+    )
+
+    if predecessor_road_target_lane_section is None:
+        return
+
+    next_lane_section_length = 0.0
+    lane_sections = utils.get_sorted_lane_sections_with_length_from_road(
+        predecessor_road
+    )
+    if predecessor_linkage.contact_point == models.ContactPoint.START:
+        next_lane_section_length = lane_sections[0].length
+    elif predecessor_linkage.contact_point == models.ContactPoint.END:
+        next_lane_section_length = lane_sections[len(lane_sections) - 1].length
+
+    _check_predecessor_with_width_zero_between_lane_sections(
+        checker_data,
+        rule_uid,
+        current_road_last_lane_section,
+        predecessor_road_target_lane_section.lane_section,
+        predecessor_linkage.contact_point,
+        next_lane_section_length,
     )
 
 
@@ -210,6 +334,76 @@ def _check_appearing_successor_junction(
                 )
 
 
+def _check_appearing_predecessor_junction(
+    checker_data: models.CheckerData,
+    rule_uid: str,
+    junction_id_map: Dict[int, etree._ElementTree],
+    road_id_map: Dict[int, etree._ElementTree],
+    road_id: int,
+    predecessor_junction_id: int,
+) -> None:
+    predecessor_connections = utils.get_connections_between_road_and_junction(
+        road_id,
+        predecessor_junction_id,
+        road_id_map,
+        junction_id_map,
+        models.ContactPoint.START,  # ROAD START == ROAD PREDECESSOR
+    )
+
+    for connection in predecessor_connections:
+        connecting_road_id = utils.get_connecting_road_id_from_connection(connection)
+        if connecting_road_id is None:
+            continue
+
+        connection_road = road_id_map.get(connecting_road_id)
+
+        if connection_road is None:
+            continue
+
+        contact_lane_sections = (
+            utils.get_incoming_and_connection_contacting_lane_sections(
+                connection, road_id_map
+            )
+        )
+
+        if contact_lane_sections is None:
+            continue
+
+        lane_links = utils.get_lane_links_from_connection(connection)
+
+        for lane_link in lane_links:
+            from_lane_id = utils.get_from_attribute_from_lane_link(lane_link)
+            to_lane_id = utils.get_to_attribute_from_lane_link(lane_link)
+
+            if from_lane_id is None or to_lane_id is None:
+                continue
+
+            connection_lane = utils.get_lane_from_lane_section(
+                contact_lane_sections.connection, to_lane_id
+            )
+
+            connection_lane_start_width = utils.evaluate_lane_width(
+                connection_lane, 0.0
+            )
+
+            if (
+                connection_lane_start_width is not None
+                and abs(connection_lane_start_width) < FLOAT_COMPARISON_THRESHOLD
+            ):
+                current_road_lane = utils.get_lane_from_lane_section(
+                    contact_lane_sections.incoming, from_lane_id
+                )
+                if current_road_lane is None:
+                    continue
+                _raise_issue(
+                    checker_data,
+                    rule_uid,
+                    current_road_lane,
+                    connection_lane,
+                    IssueSeverity.ERROR,
+                )
+
+
 def _check_road_lane_link_new_lane_appear(
     checker_data: models.CheckerData, rule_uid: str
 ) -> None:
@@ -225,14 +419,18 @@ def _check_road_lane_link_new_lane_appear(
             _check_appearing_successor_road(
                 checker_data, rule_uid, road_id_map, road_id, successor_road_id
             )
-        else:
-            successor_junction_id = utils.get_linked_junction_id(
-                road, models.LinkageTag.SUCCESSOR
+
+        predecessor_road_id = utils.get_predecessor_road_id(road)
+
+        if predecessor_road_id is not None:
+            _check_appearing_predecessor_road(
+                checker_data, rule_uid, road_id_map, road_id, predecessor_road_id
             )
 
-            if successor_junction_id is None:
-                continue
-
+        successor_junction_id = utils.get_linked_junction_id(
+            road, models.LinkageTag.SUCCESSOR
+        )
+        if successor_junction_id is not None:
             _check_appearing_successor_junction(
                 checker_data,
                 rule_uid,
@@ -240,6 +438,20 @@ def _check_road_lane_link_new_lane_appear(
                 road_id_map,
                 road_id,
                 successor_junction_id,
+            )
+
+        predecessor_junction_id = utils.get_linked_junction_id(
+            road, models.LinkageTag.PREDECESSOR
+        )
+
+        if predecessor_junction_id is not None:
+            _check_appearing_predecessor_junction(
+                checker_data,
+                rule_uid,
+                junction_id_map,
+                road_id_map,
+                road_id,
+                predecessor_junction_id,
             )
 
 
