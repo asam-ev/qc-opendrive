0ddd278d271c51b8c5d0af43d855c296c38170fd
Hoang Tung Dinh <hoangtungdinh@users.noreply.github.com>
2024-09-17 15:25:16 +0200

    Add exception logging (#100)
    Signed-off-by: hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>
    Co-authored-by: hoangtungdinh <11166240+hoangtungdinh@users.noreply.github.com>

--- qc_opendrive/main.py
+++ qc_opendrive/main.py
@@ -122,6 +122,8 @@ def execute_checker(
             constants.BUNDLE_NAME, checker.CHECKER_ID, f"Error: {str(e)}."
         )
 
+        logging.exception(f"An error occur in {checker.CHECKER_ID}.")
+
 
 def run_checks(config: Configuration, result: Result) -> None:
     checker_data = models.CheckerData(
